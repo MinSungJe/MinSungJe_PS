913876f4567a119d378b98cc8cb2ed3765f428e7
MinSungJe <minje813@naver.com>
2023-10-10 19:42:02 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input())
+if score >= 90:
+    result = 'A'
+elif score >= 80:
+    result = 'B'
+elif score >= 70:
+    result = 'C'
+elif score >= 60:
+    result = 'D'
+else:
+    result = 'F'
+print(result)
