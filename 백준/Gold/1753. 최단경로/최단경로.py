efca82d416c3980f58b6458d6d4f74ac6862ad1a
MinSungJe <minje813@naver.com>
2023-10-15 18:13:35 +0900

    [Gold IV] Title: 최단경로, Time: 556 ms, Memory: 164428 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,43 @@
+# 빠른 입력 및 모듈 불러오기
+import heapq, sys
+def input(): return sys.stdin.readline().rstrip()
+
+# 입력부
+V, E = map(int, input().split())
+start = int(input())
+
+# 초기값 설정
+heap = [(0, start)]
+INF = 200001
+result = [INF for _ in range(V+1)]
+result[start] = 0
+graph = [list() for _ in range(V+1)]
+
+# 그래프 입력
+for i in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((v,w))
+
+# 다익스트라 시작
+while heap:
+    dist, current = heapq.heappop(heap)
+
+    # 탐색 불가 조건 : 현재 dist는 기록된 dist보다 거리가 김
+    if dist > result[current]: continue
+
+    # 다음 탐색
+    for next, value in graph[current]:
+        dist_ = dist + value
+
+        # 다음 탐색 불가 조건 : 다음 dist는 기록된 dist보다 거리가 김
+        if dist_ > result[next]: continue
+        
+        # 탐색
+        result[next] = dist_
+
+        # heap에 넣기
+        heapq.heappush(heap, (dist_, next))
+
+# 출력부
+for ans in result[1:]:
+    print(ans if ans != INF else "INF")
