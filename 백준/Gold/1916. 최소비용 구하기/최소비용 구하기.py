592d87530f3f9722e67498404cab847bfa174ad7
MinSungJe <minje813@naver.com>
2023-10-18 20:53:44 +0900

    [Gold V] Title: 최소비용 구하기, Time: 328 ms, Memory: 55324 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,49 @@
+# 빠른 입력 및 모듈 불러오기
+import sys, heapq
+def input(): return sys.stdin.readline().rstrip()
+
+# 입력부
+N = int(input())
+M = int(input())
+
+# 초기값 선언
+INF = 100000001
+result = [0] + [INF for _ in range(N)]
+graph = [list() for _ in range(N+1)]
+queue = []
+
+# 그래프 입력
+for _ in range(M):
+    u, v, w = map(int, input().split())
+    graph[u].append((w, v)) # 비용이 heap의 기준이므로 비용 먼저 기록
+
+# 출발지점, 목표지점 기록
+start, end = map(int, input().split())
+result[start] = 0
+
+# heap 초기값 입력
+for w, v in graph[start]:
+    heapq.heappush(queue, (w, v))
+    result[v] = min(w, result[v])
+
+# 다익스트라
+while queue:
+    W, node = heapq.heappop(queue)
+
+    # 탐색 불가 조건 : 값이 최단거리가 아님
+    if W > result[node]: continue
+
+    # 다음 탐색
+    for value, node_ in graph[node]:
+        W_ = W + value
+        # 탐색 불가 조건 : 값이 최단거리가 아님
+        if W_ >= result[node_]: continue
+
+        # 탐색
+        result[node_] = W_
+
+        # queue에 값 입력
+        heapq.heappush(queue, (W_, node_))
+
+# 출력부
+print(result[end] if start != end else 0)
