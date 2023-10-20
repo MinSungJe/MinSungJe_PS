89db7e75ca861567a5ae8c2670fa127364b0ece7
MinSungJe <minje813@naver.com>
2023-10-20 17:26:15 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 5888 ms, Memory: 148484 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -23,28 +23,30 @@ while queue:
         result = count
         break
 
-    # 탐색 불가 조건
-    # 1. 탐색하려는 위치가 범위를 벗어남
-    if X < 0 or X >= N or Y < 0 or Y >= M: continue
-    # 2. 탐색하려는 위치는 벽임, isBreak가 False인 경우 통과 가능
-    if Map[X][Y] == '1':
-        if isBreak: continue
-        else: isBreak = True
-    # 3. 탐색하려는 위치는 이미 탐색한 적이 있음, isBreak 여부에 따라 참고하는 위치가 다름
-    if isBreak:
-        if visited[X][Y][1]: continue
-    else:
-        if visited[X][Y][0]: continue
-
-    # 탐색
-    if isBreak: visited[X][Y][1] = True
-    else: visited[X][Y][0] = True
-
     # 다음 탐색
     for i in range(4):
+        b = isBreak # for문 안에서 벽을 부쉈는지 여부가 바뀌므로 초기변수를 저장
         X_ = X + dx[i]
         Y_ = Y + dy[i]
-        queue.append((X_,Y_,count+1,isBreak))
+
+        # 탐색 불가 조건
+        # 1. 탐색하려는 위치가 범위를 벗어남
+        if X_ < 0 or X_ >= N or Y_ < 0 or Y_ >= M: continue
+        # 2. 탐색하려는 위치는 벽임, isBreak가 False인 경우 통과 가능
+        if Map[X_][Y_] == '1':
+            if b: continue
+            else: b = True
+        # 3. 탐색하려는 위치는 이미 탐색한 적이 있음, isBreak 여부에 따라 참고하는 위치가 다름
+        if b:
+            if visited[X_][Y_][1]: continue
+        else:
+            if visited[X_][Y_][0]: continue
+
+        # 탐색
+        if b: visited[X_][Y_][1] = True
+        else: visited[X_][Y_][0] = True
+
+        queue.append((X_,Y_,count+1,b))
 
 # 출력부
 print(result)
