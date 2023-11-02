17d15157f84df814f5b1c4ade6acacabae7361e4
MinSungJe <minje813@naver.com>
2023-11-03 01:50:40 +0900

    [Gold III] Title: 파티, Time: 60 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -8,17 +8,20 @@ N, M, X = map(int, input().split())
 # 초기값 선언
 INF = 100001
 graph = [list() for _ in range(N+1)]
+rGraph = [list() for _ in range(N+1)]
 
 # 그래프 입력
 for _ in range(M):
     S, E, C = map(int, input().split())
     graph[S].append((C, E))
+    rGraph[E].append((C, S))
+
 
 # 다익스트라 알고리즘
-def dijkstra(start, end):
+def dijkstra(Graph):
     result = [0] + [INF for _ in range(N)]
-    heap = [(0,start)]
-    result[start] = 0
+    heap = [(0,X)]
+    result[X] = 0
 
     # 알고리즘 시작
     while heap:
@@ -28,7 +31,7 @@ def dijkstra(start, end):
         if dist > result[node]: continue
 
         # 다음 탐색
-        for value, next in graph[node]:
+        for value, next in Graph[node]:
             dist_ = dist + value
             # 계산된 거리는 기록된 거리보다 짧지 않음
             if dist_ >= result[next]: continue
@@ -38,13 +41,16 @@ def dijkstra(start, end):
             # heap에 다음 값 집어 넣음
             heapq.heappush(heap, (dist_, next))
     
-    return result[end]
+    return result
 
+# 일반 그래프와 뒤집힌 그래프 결과 배열 가져오기
+path = dijkstra(graph)
+rPath = dijkstra(rGraph)
 
-# 모든 노드 다익스트라 검사
+# 최댓값 계산
 answer = 0
-for idx in range(1,N+1):
-    answer = max(answer, dijkstra(idx, X) + dijkstra(X, idx))
+for i in range(1,N+1):
+    answer = max(answer, path[i]+rPath[i])
 
 # 출력부
 print(answer)
