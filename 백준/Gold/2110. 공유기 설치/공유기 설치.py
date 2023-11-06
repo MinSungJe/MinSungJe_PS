ebf6477b89940cf1a43f674ceceb2e9cb46cd367
MinSungJe <minje813@naver.com>
2023-11-06 16:39:41 +0900

    [Gold IV] Title: 공유기 설치, Time: 280 ms, Memory: 40412 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,43 @@
+# 빠른 입력
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+# 입력부
+N, C = map(int, input().split())
+Map = []
+for _ in range(N):
+    Map.append(int(input()))
+
+# 집 위치 정렬
+Map.sort()
+
+# 최대 거리를 인자로 받고 배치가 가능한지 여부를 확인하는 함수
+def install(dist):
+    lastPos = Map[0]
+    count = 1
+    for pos in Map[1:]:
+        if pos >= lastPos+dist:
+            count += 1
+            lastPos = pos
+            if count == C: return True
+    return False
+
+# 이분탐색 알고리즘
+def BS():
+    l = 0
+    r = Map[-1] - Map[0] + 1
+    result = 0
+
+    while l < r:
+        m = (l+r)//2
+        if install(m):
+            l = m + 1
+            result = m
+        else:
+            r = m
+
+    # 출력부
+    print(result)
+
+# 함수 실행
+BS()
