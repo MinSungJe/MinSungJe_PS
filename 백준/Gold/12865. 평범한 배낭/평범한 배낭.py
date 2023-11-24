174dc1310aee857bc73a6cc3548b0744c0af408e
MinSungJe <minje813@naver.com>
2023-11-25 01:47:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 4820 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,10 +1,19 @@
-N, K = map(int, input().split())
-dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
+# 빠른 입력
+import sys
+def input(): return sys.stdin.readline().rstrip()
 
-for i in range(1,N+1):
+# 입력부
+N, K = map(int, input().split())
+
+# DP Table 선언
+DP = [[0 for _ in range(K+1)] for _ in range(N+1)]
+
+# DP Table 채우기
+for i in range(1, N+1):
     W, V = map(int, input().split())
-    for j in range(K+1):
-        dp[i][j] = dp[i-1][j]
-        if j >= W: dp[i][j] = max(dp[i][j], dp[i-1][j-W]+V)
-            
-print(dp[N][K])
+    for j in range(1, K+1):
+        if j < W: DP[i][j] = DP[i-1][j]
+        else: DP[i][j] = max(DP[i-1][j], DP[i-1][j-W] + V)
+
+# 출력부
+print(DP[N][K])
