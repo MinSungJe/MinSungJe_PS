ed693eb6fe0bb023c5e220c4b34d82b84ebc1132
Minje Sung <minje813@naver.com>
2024-02-03 23:11:03 +0900

    [Silver III] Title: 주유소, Time: 144 ms, Memory: 46788 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,34 +1,25 @@
-# 재귀 제한 해제
-import sys
-sys.setrecursionlimit(10**6)
-
 # 입력부
 N = int(input())
 road = list(map(int, input().split()))
-oil = list(map(int, input().split()))
+cost = list(map(int, input().split()))
 
 # 누적합 구하기
 dist = [0 for _ in range(N)]
-for i in range(1, N): dist[i] = dist[i-1] + road[i-1]
+for i in range(1,N): dist[i] = dist[i-1] + road[i-1]
 
 # 초기값 선언
-INF = (10**18) + 1
 result = 0
-DP = [0 for _ in range(N)]
+last_pos = 0
+min_cost = cost[last_pos]
 
-# DFS(+DP)
-def DFS(pos):
-    if pos == N-1: return 0
-    if DP[pos]: return DP[pos] # 메모이제이션
-    
-    # 탐색
-    result = INF
-    for i in range(pos+1, N):
-        cost = (dist[i]-dist[pos]) * oil[pos]
-        result = min(result, cost+DFS(i))
-    DP[pos] = result # 메모이제이션
-    return result
+for pos in range(1, N):
+    if min_cost >= cost[pos]: # 값이 비싸지지 않은 경우 지불(그리디)
+        result += (dist[pos]-dist[last_pos]) * min_cost
+        min_cost = cost[pos]
+        last_pos = pos
+
+# 계산이 되지 않은 부분 더해주기
+result += (dist[N-1]-dist[last_pos]) * min_cost
 
 # 출력부
-result = DFS(0)
 print(result)
