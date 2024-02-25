9f3943ce0b08e79fd252d77f2497f92d6175fc5d
Minje Sung <minje813@naver.com>
2024-02-25 22:13:27 +0900

    [Silver I] Title: 점프, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -1,4 +1,4 @@
-# 빠른 입력 및 재귀 제한 해제
+# 빠른 입력
 import sys
 def input(): return sys.stdin.readline().rstrip()
 
@@ -7,26 +7,15 @@ N = int(input())
 Map = [list(map(int, input().split())) for _ in range(N)]
 
 # 초기값 선언
-DP = [[-1 for _ in range(N)] for _ in range(N)]
+DP = [[0 for _ in range(N)] for _ in range(N)]
+DP[0][0] = 1
 
-# DFS(+DP)
-def DFS(X, Y):
-    # 도착
-    if X == N-1 and Y == N-1: return 1
+# Bottom-Up
+for i in range(N):
+    for j in range(N):
+        if Map[i][j]:
+            if i+Map[i][j] < N: DP[i+Map[i][j]][j] += DP[i][j]
+            if j+Map[i][j] < N: DP[i][j+Map[i][j]] += DP[i][j]
 
-    # 메모이제이션
-    if DP[X][Y] != -1: return DP[X][Y]
-
-    # 다음 탐색
-    result = 0
-    if Map[X][Y]:
-        X_, Y_ = X+Map[X][Y], Y+Map[X][Y]
-        if X_ < N: result += DFS(X_, Y)
-        if Y_ < N: result += DFS(X, Y_)
-
-    DP[X][Y] = result
-    return result
-
-# 함수 호출 및 출력부
-result = DFS(0, 0)
-print(result)
+# 출력부
+print(DP[N-1][N-1])
