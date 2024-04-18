5c098bd5c86ab66ba427c8350810cea4b3e9c527
Minje Sung <minje813@dgu.ac.kr>
2024-04-18 13:27:33 +0900

    [Gold IV] Title: 4연산, Time: 72 ms, Memory: 34148 KB -BaekjoonHub

---
+++ 백준/Gold/14395. 4연산/4연산.py
@@ -0,0 +1,32 @@
+# 모듈 불러오기
+from collections import deque
+
+# 입력부
+s, t = map(int, input().split())
+
+# 초기값 선언
+result = -1
+operator = ['*', '+']
+queue = deque([(s*s, '*'), (s+s, '+'), (1, '/')])
+
+# BFS
+while queue:
+    node, cmd = queue.popleft()
+
+    # 탐색 불가 조건
+    if node > t: continue
+
+    # 탐색
+    if node == t:
+        result = cmd
+        break
+
+    # 다음 탐색
+    for i in range(2):
+        if i == 0: node_ = (node*node)
+        if i == 1: node_ = (node+node)
+        if node == node_: continue
+        queue.append((node_, cmd+operator[i]))
+
+# 출력부
+print(result if s != t else 0)
