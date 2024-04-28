ff147a8268acbac86423689d7a5bde179d67fb32
Minje Sung <minje813@dgu.ac.kr>
2024-04-28 16:53:46 +0900

    [Gold IV] Title: 오큰수, Time: 1320 ms, Memory: 152816 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,28 @@
+# 모듈 불러오기
+from collections import deque
+
+# 입력부
+N = int(input())
+A = list(map(int, input().split()))
+
+# 초기값 선언
+stack = deque()
+result = [0 for _ in range(N)]
+
+# 결과값 채우기
+for i in range(N-1, -1, -1):
+    if not stack: result[i] = -1
+    else:
+        while stack:
+            value = stack.pop()
+            if A[i] < value:
+                result[i] = value
+                stack.append(value)
+                break
+        
+        if not stack: result[i] = -1
+    
+    stack.append(A[i])
+
+# 출력부
+print(*result)
