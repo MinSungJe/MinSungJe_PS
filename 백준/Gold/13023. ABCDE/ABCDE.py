b9ef8d58939f7010283c0bde9cbb6b0e0997259d
Minje Sung <minje813@dgu.ac.kr>
2024-06-13 16:18:55 +0900

    [Gold V] Title: ABCDE, Time: 3176 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,40 @@
+# 빠른 입력 및 재귀 제한 해제
+import sys
+def input(): return sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**6)
+
+# 입력부
+N, M = map(int, input().split())
+graph = [list() for _ in range(N)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# DFS
+def DFS(node, count):
+    # 탐색 불가 조건
+    if visited[node]: return 0
+    if count >= 5: return 1
+
+    # 탐색
+    visited[node] = True
+    
+    # 다음 탐색
+    result = 1
+    for node_ in graph[node]:
+        result = max(result, 1+DFS(node_, count+1))
+
+    # 백트래킹
+    visited[node] = False
+    return result
+
+# 함수 호출 및 출력부
+result = 0
+visited = [False for _ in range(N)]
+for i in range(N):
+    depth = DFS(i, 1)
+    if depth >= 5:
+        result = 1
+        break
+print(result)
