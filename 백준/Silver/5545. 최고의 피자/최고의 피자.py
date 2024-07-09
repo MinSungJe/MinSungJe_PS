4b2b1205c306347e09a3ada61422464f8f82d158
Minje Sung <minje813@dgu.ac.kr>
2024-07-09 19:58:14 +0900

    [Silver III] Title: 최고의 피자, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/5545. 최고의 피자/최고의 피자.py
@@ -0,0 +1,33 @@
+# 빠른 입력
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+# 입력부
+N = int(input())
+A, B = map(int, input().split())
+C = int(input())
+D = [int(input()) for _ in range(N)]
+
+# 토핑 열량 정렬
+D.sort(reverse=True)
+
+# 현재 열량비 구하기
+def getResult(energy, price): return int((energy/price) // 1)
+
+# 초기값 선언
+energy = C
+price = A
+result = getResult(energy, price)
+
+# 열량 높은 토핑부터 넣어보기
+for i in range(N):
+    energy += D[i]
+    price += B
+    value = getResult(energy, price)
+
+    # 열량비가 더 커졌으면 더 좋은 피자임
+    if value >= result: result = value
+    else: break
+
+# 출력부
+print(result)
