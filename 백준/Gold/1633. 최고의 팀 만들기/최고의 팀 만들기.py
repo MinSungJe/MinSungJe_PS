5d360a7e0d7d1e0721f030442dd8f36935cefc0f
Minje Sung <minje813@dgu.ac.kr>
2024-08-08 17:40:31 +0900

    [Gold IV] Title: 최고의 팀 만들기, Time: 116 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/1633. 최고의 팀 만들기/최고의 팀 만들기.py
+++ 백준/Gold/1633. 최고의 팀 만들기/최고의 팀 만들기.py
@@ -13,39 +13,16 @@ while True:
 
 # 초기값 선언
 N = len(team)
-DP = [[[0 for _ in range(N)] for _ in range(16)] for _ in range(16)]
-INF = 100001
+DP = [[[0 for _ in range(16)] for _ in range(16)] for _ in range(N)]
 
-# DFS(+DP)
-def DFS(W, B, idx):
-    # 탐색 종료
-    if W == 15 and B == 15: return 0
-    if idx == N: return -INF
+# Bottom-Up DP
+for i in range(N):
+    for j in range(16):
+        for k in range(16):
+            DP[i][j][k] = DP[i-1][j][k] # 추가하지 않은 경우 반영
+            if j > 0: DP[i][j][k] = max(DP[i][j][k], DP[i-1][j-1][k] + team[i][0]) # 백으로 추가
+            if k > 0: DP[i][j][k] = max(DP[i][j][k], DP[i-1][j][k-1] + team[i][1]) # 흑으로 추가
 
-    # 메모이제이션
-    if DP[W][B][idx]: return DP[W][B][idx]
-
-    # 다음 탐색
-    result = 0
-    
-    # 이 선수를 W로 선정
-    if W < 15:
-        value = team[idx][0] + DFS(W+1, B, idx+1)
-        result = max(result, value)
-
-    # 이 선수를 B로 선정
-    if B < 15:
-        value = team[idx][1] + DFS(W, B+1, idx+1)
-        result = max(result, value)
-
-    # 이 선수를 아무것도 선정하지 않음
-    value = DFS(W, B, idx+1)
-    result = max(result, value)
-
-    # 메모
-    DP[W][B][idx] = result
-    return result
-
-# 함수 호출 및 출력부
-result = DFS(0, 0, 0)
+# 출력부
+result = DP[N-1][15][15]
 print(result)
