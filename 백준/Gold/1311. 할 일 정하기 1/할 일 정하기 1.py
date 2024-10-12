be416ced67f649d252883a56dc9eedbfe881cb53
Minje Sung <minje813@dgu.ac.kr>
2024-10-12 13:06:52 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 4752 ms, Memory: 72136 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.py
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.py
@@ -4,14 +4,14 @@ D = [list(map(int, input().split())) for _ in range(N)]
 
 # 초기값 선언
 visited = 0
-DP = [[0 for _ in range((1<<N)-1)] for _ in range(N)]
+DP = [0 for _ in range((1<<N)-1)]
 INF = 200001
 
 # DFS(+DP)
 def DFS(idx, visited):
     # 탐색 종료
     if idx == N: return 0
-    if DP[idx][visited]: return DP[idx][visited] # 메모이제이션
+    if DP[visited]: return DP[visited] # 메모이제이션
 
     # 다음 탐색
     result = INF
@@ -20,7 +20,7 @@ def DFS(idx, visited):
         if visited & (1 << i): continue
         result = min(result, D[idx][i]+DFS(idx+1, (visited|1<<i)))
 
-    DP[idx][visited] = result
+    DP[visited] = result
     return result
 
 # 함수 호출 및 출력부
