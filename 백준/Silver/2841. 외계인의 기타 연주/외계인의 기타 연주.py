9dcb4ca43b98a0c2ecb8cb94a1e77eaade0f247a
Minje Sung <minje813@dgu.ac.kr>
2024-12-03 15:46:22 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 680 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -1,25 +1,25 @@
 # 빠른 입력 및 모듈 불러오기
-import sys, heapq
+from collections import deque
+import sys
 def input(): return sys.stdin.readline().rstrip()
 
 # 입력부
 N, P = map(int, input().split())
 
 # 초기값 선언
-finger = [list() for _ in range(7)]
+finger = [deque() for _ in range(7)]
 
 # 플랫 짚기
 result = 0
 for _ in range(N):
     line, flat = map(int, input().split())
-    flat *= -1
     
-    while finger[line] and finger[line][0] < flat:
-        heapq.heappop(finger[line])
+    while finger[line] and finger[line][0] > flat:
+        finger[line].popleft()
         result += 1
     
     if finger[line] and finger[line][0] == flat: continue
-    heapq.heappush(finger[line], flat)
+    finger[line].appendleft(flat)
     result += 1
 
 # 출력부
