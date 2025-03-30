a9d244e23e66674c886a51ced1c9fabd31169c50
Minje Sung <minje813@dgu.ac.kr>
2025-03-30 16:28:27 +0900

    [Silver IV] Title: Rainbow Beads, Time: 124 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17164. Rainbow Beads/Rainbow Beads.py
+++ 백준/Silver/17164. Rainbow Beads/Rainbow Beads.py
@@ -1,26 +1,16 @@
 # 입력부
 N = int(input())
-jewel_original = input()
-
-# 두 색맹에게 보이는 보석 배치 만들기
-jewel_red, jewel_blue = '', ''
-for jewel in jewel_original:
-    if jewel == 'V':
-        jewel_red += 'R'
-        jewel_blue += 'B'
-    else:
-        jewel_red += jewel
-        jewel_blue += jewel
+jewel = input()
 
 # 반복문을 돌며 확인
 result = 1
 value = 1
 for i in range(N-1):
-    if jewel_red[i] == jewel_red[i+1] or jewel_blue[i] == jewel_blue[i+1]:
-        value = 1
+    if (jewel[i] == "R" and jewel[i+1] == "B") or (jewel[i] == "B" and jewel[i+1] == "R"):
+        value += 1
+        result = max(result, value)
         continue
-    value += 1
-    result = max(result, value)
+    value = 1
 
 # 출력부
 print(result)
