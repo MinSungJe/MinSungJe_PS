26d8e5de7688062b13d1476bf2e965905bb9f513
Minje Sung <minje813@dgu.ac.kr>
2025-04-22 15:08:50 +0900

    [Silver III] Title: Cakey McCakeFace, Time: 2876 ms, Memory: 367264 KB -BaekjoonHub

--- 백준/Silver/15410. Cakey McCakeFace/Cakey McCakeFace.py
+++ 백준/Silver/15410. Cakey McCakeFace/Cakey McCakeFace.py
@@ -1,27 +1,30 @@
-from collections import Counter
-
+# 입력부
 N = int(input())
 M = int(input())
 entry_detector = list(map(int, input().split()))
 exit_detector = list(map(int, input().split()))
 
-gap_counter = Counter()
+# 초기값 선언
+gap_count = dict()
 
-exit_set = set(exit_detector)
-
-# 모든 가능한 gap의 빈도를 계산
+# N과 M 사이 차이값 후보 구하기
 for entry_time in entry_detector:
     for exit_time in exit_detector:
-        if entry_time <= exit_time:
-            gap = exit_time - entry_time
-            gap_counter[gap] += 1
+        if entry_time > exit_time: continue
+        gap = exit_time - entry_time
+        if gap in gap_count.keys(): gap_count[gap] += 1
+        else: gap_count[gap] = 1
 
-# 최댓값 가지는 gap 찾기
+# 결과값 구하기
 result = 0
 max_value = 0
-for gap, count in gap_counter.items():
-    if count > max_value or (count == max_value and gap < result):
-        max_value = count
-        result = gap
+for key, value in gap_count.items():
+    if value < max_value: continue
+    if max_value == value and result < key: continue
+    
+    # 갱신
+    max_value = value
+    result = key
 
+# 출력부
 print(result)
