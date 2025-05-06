c5b3edf2f396f8bc8552db2e7503f3c3465b81df
Minje Sung <minje813@dgu.ac.kr>
2025-05-06 14:36:26 +0900

    [Gold IV] Title: 소트 게임, Time: 372 ms, Memory: 40068 KB -BaekjoonHub

---
+++ 백준/Gold/1327. 소트 게임/소트 게임.py
@@ -0,0 +1,50 @@
+# 모듈 불러오기
+from collections import deque, defaultdict
+
+# 입력부
+N, K = map(int, input().split())
+numbers = ''.join(input().split())
+
+# 뒤집기 함수
+def flip(numbers, idx):
+    return numbers[:idx] + get_reversed_string(numbers[idx:idx+K]) + numbers[idx+K:]
+
+def get_reversed_string(letter):
+    result = ''
+    for l in letter: result = l + result 
+    return result
+
+# 오름차순 확인 함수
+def check_is_asc(numbers):
+    prev_value = 0
+    for number in numbers:
+        value = int(number)
+        if prev_value > value: return False
+        prev_value = value
+    return True
+
+# 초기값 선언
+queue = deque([(numbers, 0)])
+visited = defaultdict(bool)
+
+# BFS
+result = -1
+while queue:
+    node, count = queue.popleft()
+
+    # 탐색 불가 조건
+    if visited[node]: continue
+
+    # 탐색
+    if check_is_asc(node):
+        result = count
+        break
+    visited[node] = True
+
+    # 다음 탐색
+    for idx in range(0, N-K+1):
+        node_ = flip(node, idx)
+        queue.append((node_, count+1))
+
+# 출력부
+print(result)
