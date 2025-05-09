4d4cf8b035247022c7666f9b108adce7bb03c780
Minje Sung <minje813@dgu.ac.kr>
2025-05-09 14:20:50 +0900

    [Silver II] Title: 점프 점프, Time: 56 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/14248. 점프 점프/점프 점프.py
+++ 백준/Silver/14248. 점프 점프/점프 점프.py
@@ -1,29 +1,24 @@
-# 모듈 불러오기
-from collections import deque
-
 # 입력부
 n = int(input())
 A = list(map(int, input().split()))
 s = int(input())
 
 # 초기값 선언
-queue = deque([s-1])
 visited = [0 for _ in range(n)]
 
-# BFS
-while queue:
-    node = queue.popleft()
-
+# DFS
+def DFS(node):
     # 탐색 불가 조건
-    if node < 0 or node >= n: continue
-    if visited[node]: continue
+    if node < 0 or node >= n: return
+    if visited[node] == 1: return
 
     # 탐색
     visited[node] = 1
 
     # 다음 탐색
     value = A[node]
-    for node_ in (node+value, node-value): queue.append(node_)
+    for node_ in (node+value, node-value): DFS(node_)
 
-# 출력부
+# 함수 호출 및 출력부
+DFS(s-1)
 print(sum(visited))
