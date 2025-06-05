c32b3b1630c6a2338c4fa90f343ad132feb3661e
Minje Sung <minje813@dgu.edu>
2025-06-05 15:08:03 +0900

    [Silver II] Title: 비밀번호, Time: 3720 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/13908. 비밀번호/비밀번호.py
+++ 백준/Silver/13908. 비밀번호/비밀번호.py
@@ -2,17 +2,28 @@
 n, m = map(int, input().split())
 if m != 0 : numbers = list(map(int, input().split()))
 
-# 번호 전부 확인
-result = 0
-for i in range(10 ** n):
-    numberSet = set(map(int, list(f'{i:0{n}}')))
-    
-    # 숫자가 있는지 확인
-    canPassword = True
-    if m != 0:
-        for number in numbers:
-            if not number in numberSet: canPassword = False
-    if canPassword: result += 1
+# 초기값 선언
+visited = [0 for _ in range(10)]
 
-# 출력부
+# DFS
+def DFS(idx):
+    # 탐색 종료
+    if idx == n:
+        isPassword = True
+        if m != 0:
+            for number in numbers:
+                if not visited[number] != 0: isPassword = False        
+        return 1 if isPassword else 0
+    
+    # 다음 탐색
+    result = 0
+    for digit in range(10):
+        visited[digit] += 1
+        result += DFS(idx+1)
+        visited[digit] -= 1
+    
+    return result
+
+# 함수 호출 및 출력부
+result = DFS(0)
 print(result)
