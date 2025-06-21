358c2a17ad58e9c0847f3ab9f0fa04dc82e14bac
Minje Sung <minje813@dgu.edu>
2025-06-21 21:54:10 +0900

    [level 3] Title: 정수 삼각형, Time: 10.54 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -2,28 +2,29 @@ function solution(triangle) {
     // 초기값 선언
     const maxRow = triangle.length;
     
-    // DP 배열 선언(기록용)
+    // DP 배열 선언
     const DP = new Array(maxRow).fill(null).map(() => new Array(maxRow).fill(-1));
     
-    // DFS(+DP)
-    function DFS(row, idx) {
-        // 탐색 종료
-        if (row === maxRow) return 0;
-        
-        // DP 배열에 이미 기록된 경우 해당 값 반환
-        if (DP[row][idx] !== -1) return DP[row][idx];
-        
-        // 다음 탐색
-        let result = 0;
-        for (let idx_ of [idx, idx+1])
-            result = Math.max(result, triangle[row][idx] + DFS(row+1, idx_))
-        
-        DP[row][idx] = result; // 메모이제이션(DP배열에 기록)
-        return result;
+    // Bottom-Up DP
+    for (let row=maxRow-1; row>=0; row--) {        
+        for (let idx=0; idx<=row; idx++) {
+            // 맨 아랫줄인 경우 자기 자신값만 기록
+            if (row === maxRow-1) {
+                DP[row][idx] = triangle[row][idx];
+                continue;
+            }
+            
+            // 그렇지 않으면 계산
+            let result = 0;
+            for (let idx_ of [idx, idx+1]) {
+                result = Math.max(result, triangle[row][idx]+DP[row+1][idx_])
+            }
+            DP[row][idx] = result;
+        }
     }
-    
-    // 함수 호출
-    var answer = DFS(0, 0);
+
+    // DP 배열로 답을 얻어냄
+    var answer = DP[0][0];
     
     return answer;
 }
