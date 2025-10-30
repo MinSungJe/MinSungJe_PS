d7dab081568d4591c162cfdb2d892e9d268d7cbc
Minje Sung <minje813@dgu.edu>
2025-10-30 11:42:59 +0900

    [Gold III] Title: ABBC, Time: 164 ms, Memory: 32704 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.py
+++ 백준/Gold/25381. ABBC/ABBC.py
@@ -3,27 +3,27 @@ S = input()
 L = len(S)
 
 # 초기값 선언
-answer = 0
-isUsedB = [False for _ in range(L)]
+canDeleteCount = 0
+BCount = 0
 canDeleteBCount = 0
 canDeleteACount = 0
 
-# B-C 확인
+# A-B 개수 확인
 for i in range(L-1, -1, -1):
-    if S[i] == 'C':
-        canDeleteBCount += 1
-    if S[i] == 'B' and canDeleteBCount > 0:
-        isUsedB[i] = True
-        canDeleteBCount -= 1
-        answer += 1
-
-# A-B 확인
-for i in range(L-1, -1, -1):
-    if S[i] == 'B' and not isUsedB[i]:
+    if S[i] == 'B':
         canDeleteACount += 1
+        BCount += 1
     if S[i] == 'A' and canDeleteACount > 0:
         canDeleteACount -= 1
-        answer += 1
+        canDeleteCount += 1
+
+# B-C 개수 확인
+for i in range(L-1, -1, -1):
+    if S[i] == 'C': canDeleteBCount += 1
+    if S[i] == 'B' and canDeleteBCount > 0:
+        canDeleteBCount -= 1
+        canDeleteCount += 1
 
 # 출력부
+answer = min(canDeleteCount, BCount)
 print(answer)
