3822c731d1f429e288e287d04957538fdbad2ef1
Minje Sung <minje813@dgu.edu>
2026-01-07 19:47:24 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+names = {
+    "NLCS": "North London Collegiate School",
+    "BHA": "Branksome Hall Asia",
+    "KIS": "Korea International School",
+    "SJA": "St. Johnsbury Academy"
+}
+
+initial_name = input()
+answer = names[initial_name]
+print(answer)
